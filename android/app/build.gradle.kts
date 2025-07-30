b29a91773bfb77eef575b3e72ba7460272fb5aa6
jasonapp2244 <jasonparker2244@gmail.com>
2025-07-30 16:09:37 -0700

    splash merger to home

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.getxmvvm"
+    namespace = "com.craft.getxmvvm"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
     compileOptions {
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.getxmvvm"
+        applicationId = "com.craft.getxmvvm"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
--- android/app/src/main/kotlin/com/example/getxmvvm/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/getxmvvm/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.getxmvvm
+package com.craft.getxmvvm
 
 import io.flutter.embedding.android.FlutterActivity
 
